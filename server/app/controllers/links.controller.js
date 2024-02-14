64dbaaf1b641830a74feeef7134bab37c424ca3a
josephdominguez <joseph.dominguez.virla@gmail.com>
2024-02-14 14:45:49 -0700

    abstracted link retrieval code

--- server/app/controllers/links.controller.js
+++ server/app/controllers/links.controller.js
@@ -1,24 +1,51 @@
 const linksModel = require('../models/links.model');
 
+/**
+ * Retrieves the original link associated with a short link and increments the click count if needed.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {boolean} [shouldIncrementClick=false] - Whether to increment the click count.
+ * @returns {Promise<void>} - A Promise that resolves once the original link is retrieved and handled.
+ */
+const retrieveOriginalLink = async (req, res, shouldIncrementClick = false) => {
+    try {
+        // Get short link from request.
+        const shortLink = req.params.shortLink || req.query.shortLink;
+        if (!shortLink) {
+            return res.status(404).json({ error: 'No short link provided' }); 
+        }
+
+        // Returns original link and increments click counter if needed.
+        const originalLink = await linksModel.getOriginalLink(shortLink);
+        if (originalLink) {
+            res.json({ originalLink });
+            if (shouldIncrementClick) {
+                await linksModel.incrementClickCount(shortLink);
+            }
+        } else {
+            res.status(404).json({ error: 'Original link not found' });
+        }
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
 exports.createShortLink = async (req, res) => {
     try {
         const { originalLink } = req.query;
         const shortLink = await linksModel.createShortLink(originalLink);
         res.json({ shortLink });
-    } catch (error) {
-        console.log(error);
+    } catch (e) {
+        console.error(e);
         res.status(500).json({ error: 'Internal server error' });
     }
 };
 
 exports.getOriginalLink = async (req, res) => {
-    const { shortLink } = req.params;
-    const originalLink = await linksModel.getOriginalLink(shortLink);
-    
-    // Redirects user to original link if it exists and increments number of clicks.
-    if (originalLink) {
-        res.json({ originalLink }); 
-        await linksModel.incrementClickCount(shortLink);
-    } 
-    else { res.status(404).json({ error: 'Shortened link not found' }); }
+    await retrieveOriginalLink(req, res);
+};
+
+exports.getOriginalLinkWithClickIncrement = async (req, res) => {
+    await retrieveOriginalLink(req, res, (shouldIncrementClick = true));
 };
