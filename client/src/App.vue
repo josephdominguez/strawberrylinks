e5f58e326c8fc960bebc0b8a42f0e340e209157b
josephdominguez <joseph.dominguez.virla@gmail.com>
2024-02-14 19:01:05 -0700

    created page loading component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import PageLoader from '@/components/layout/PageLoader.vue';
+  import { useAuth0 } from '@auth0/auth0-vue';
+
+  const { isLoading } = useAuth0();
+</script>
 
 <template>
-  <router-view />
+  <div v-if="isLoading">
+    <PageLoader />
+  </div>
+  <router-view v-else />
 </template>
---
+++ client/src/components/layout/PageLoader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="loader"></div>
+</template>
+
+<style scoped>
+.loader {
+  margin: auto;
+  border: 4px solid rgba(200, 200, 200, 0.3);
+  border-top: 4px solid var(--strawberry);
+  border-radius: 50%;
+  width: 32px;
+  height: 32px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
