41bdae42416f286ecb3362955e7fe5a500664afb
josephdominguez <joseph.dominguez.virla@gmail.com>
2024-02-14 19:00:31 -0700

    added auth0 to vue.js app

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,24 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { createAuth0 } from '@auth0/auth0-vue';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+app.use(
+    createAuth0({
+        domain: import.meta.env.VITE_AUTH0_DOMAIN,
+        clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+        authorizationParams: {
+            redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+        },
+    })
+);
 
-app.mount('#app')
+app.mount('#app');
