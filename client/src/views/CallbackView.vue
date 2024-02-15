fd8f9609d64682c197adaae12c3908c5d2b6ecec
josephdominguez <joseph.dominguez.virla@gmail.com>
2024-02-14 19:03:30 -0700

    created callback page

---
+++ client/src/views/CallbackView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import PageLayout from '@/components/layout/PageLayout.vue';
+import PageLoader from '@/components/layout/PageLoader.vue';
+</script>
+
+<template>
+  <PageLayout>
+    <PageLoader />
+  </PageLayout>
+</template>
