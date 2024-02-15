f06ebcfda0dd999d0256d4910390284aa1774284
josephdominguez <joseph.dominguez.virla@gmail.com>
2024-02-14 19:03:11 -0700

    genericized copy button button

---
+++ client/src/components/link/CopyLinkButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { link } = defineProps(['link']);
+
+const copyToClipboard = () => {
+  const el = document.createElement('textarea');
+  el.value = link;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+</script>
+
+<template>
+  <button @click="copyToClipboard" class="copy-button"><i class="fas fa-copy"></i> Copy</button>
+</template>
+
+<style scoped>
+.copy-button {
+  padding: 8px;
+}
+</style>
