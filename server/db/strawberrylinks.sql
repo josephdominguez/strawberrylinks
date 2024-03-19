7ff043eb08fc2bfb52a7a6013b8bff8d8a04e607
josephdominguez <joseph.dominguez.virla@gmail.com>
2024-03-18 23:36:09 -0600

    updated schema to accomodate auth0 users

--- server/db/strawberrylinks.sql
+++ server/db/strawberrylinks.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
-  sub SERIAL PRIMARY KEY,     -- Auth0 user identifier (sub claim in JWT)
+  sub VARCHAR(255) PRIMARY KEY,     -- Auth0 user identifier (sub claim in JWT)
   email VARCHAR(255) NOT NULL,    -- User's email address
 );
 
@@ -12,19 +12,10 @@ DROP TABLE IF EXISTS links CASCADE;
 CREATE TABLE links (
   link_id SERIAL PRIMARY KEY,     -- Unique identifier for each link
   original_link TEXT NOT NULL,    -- The original (long) URL
-  short_link TEXT NOT NULL,   -- The shortened version of the URL
+  short_link TEXT NOT NULL UNIQUE,   -- The shortened version of the URL
   clicks INT DEFAULT 0,           -- Count of clicks, initialized to 0
   date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp of link creation (current time by default)
   permanent BOOLEAN NOT NULL DEFAULT TRUE, -- Indicates if the link is permanent (true by default)
   expiration_date TIMESTAMP DEFAULT NULL, -- Timestamp of link expiration date (null by default)
-  sub INT REFERENCES users(sub) -- Reference to the user who created the link
-);
-
--- Creates the UserLinks table.
-DROP TABLE IF EXISTS userlinks CASCADE;
-
-CREATE TABLE userlinks (
-  user_link_id SERIAL PRIMARY KEY,          -- Unique identifer for user-link relationships
-  sub INT REFERENCES users(sub),    -- Reference to the user who created the link
-  link_id INT REFERENCES links(link_id)     -- Reference to the shortened link associated with the user
+  sub VARCHAR(255) DEFAULT NULL REFERENCES users(sub) -- Reference to the user who created the link (null by default)
 );
